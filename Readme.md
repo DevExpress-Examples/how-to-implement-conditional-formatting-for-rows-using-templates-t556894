18b12d9f8b057e05a1c51ff736b621222b328de4
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to: Implement Conditional Formatting for Rows Using Templates
+
+
+This example illustrates how to use templates to alternate the row background color.
+
+<br/>
+
+
---
+++ VB/Using_Templates/MainPage.xaml.vb
@@ -0,0 +1,25 @@
+﻿Partial Public NotInheritable Class MainPage
+    Inherits Page
+    Public Sub New()
+        Me.InitializeComponent()
+        grid.ItemsSource = New Data.ProductList()
+    End Sub
+End Class
+
+Public Class RowHandleToBackgroundConverter
+    Implements IValueConverter
+    Public Property EvenRowBackground() As Brush
+    Public Property OddRowBackground() As Brush
+
+    Private Function IValueConverter_Convert(value As Object, targetType As Type, parameter As Object, language As String) As Object Implements IValueConverter.Convert
+        If (DirectCast(value, Integer) Mod 2) = 0 Then
+            Return OddRowBackground
+        Else
+            Return EvenRowBackground
+        End If
+    End Function
+
+    Private Function IValueConverter_ConvertBack(value As Object, targetType As Type, parameter As Object, language As String) As Object Implements IValueConverter.ConvertBack
+        Throw New NotImplementedException()
+    End Function
+End Class
